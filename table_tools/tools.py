0bcf2010ea3790b171f86167a177f3469b79d126
AeroNotix <aaron.l.france@gmail.com>
2012-06-15 14:26:41 +0200

    Sphinx docstrings

--- table_tools/tools.py
+++ table_tools/tools.py
@@ -6,6 +6,7 @@ databases and Qt applications
 import contextlib
 import MySQLdb
 
+
 def get_headings(db, query):
     '''
     get the headings from a table in the specified database
@@ -14,6 +15,9 @@ def get_headings(db, query):
     table you want to query, returns the headers.
 
     MySQL database syntax.
+
+    :param db: A :class:`MySQLdb.connect`.
+    :param query: A :class:`string` which contains the query to be executed
     '''
 
     cursor = db.cursor()
@@ -25,25 +29,32 @@ def get_headings(db, query):
     [ioheadings.append(item[0]) for item in headings]
     return ioheadings
 
+
 @contextlib.contextmanager
 def sql_query(db, query):
-
     '''
     Ability to use a set of sql results
     within a with statement.
+
+    :param db: A :class:`MySQLdb.connect`.
+    :param query: A :class:`string` which contains the query to be executed
     '''
     
     cursor = db.cursor()
     cursor.execute(query)
     yield cursor.fetchall()
 
-def itersql(db, query):
 
+def itersql(db, query):
     '''
     Yields a row of data from the query
 
     Used to iterate over query rather than querying
     then assigning, then looping over that data.
+
+    :param db: A :class:`MySQLdb.connect`.
+    :param query: A :class:`string` which contains the query to be executed
+    :returns: Yields a row from the database.
     '''
     
     cursor = db.cursor()
@@ -60,8 +71,11 @@ def to_unicode(string, encoding='utf-8'):
     '''
     Returns a string in the specified encoding,
     default is utf-8
+
+    :param string: :class:`string` which needs to be converted
+    :param encoding: :class:`string` which is a encoding type.
     '''
-    
+
     return unicode(string).encode(encoding)
 
     
