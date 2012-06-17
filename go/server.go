02f9a48476e8d118dff30e5cb6bc45a26a843718
Aaron France <aaron.l.france@gmail.com>
2012-06-17 20:11:12 +0200

    Server sends back the metadata field when queried with a URL such as \'domain/param/db_name\'. This is a very basic api for retrieving metadata about a table and the metadata needs to be inserted manually for each table. When there will be an table creation interface/api then this will be automatically created.

--- go/server.go
+++ go/server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/hoisie/web"
 	mysql "github.com/ziutek/mymysql/mysql"
 	_ "github.com/ziutek/mymysql/native"
@@ -9,14 +8,19 @@ import (
 
 func databaseParameters(ctx *web.Context, dbname string) string {
 
+	if dbname == "" {
+		return "Error: No database selected"
+	}
+
 	db := mysql.New(
 		"tcp",
 		"",
 		"127.0.0.1:3306",
 		"aero",
-		"--------",
+		"---------",
 		"db_freepoint",
 	)
+
 	err := db.Connect()
 	if err != nil {
 		panic(err)
@@ -24,7 +28,6 @@ func databaseParameters(ctx *web.Context, dbname string) string {
 
 	// need to find the parameterization syntax
 	sqlStr := "SELECT * FROM tbl_metadata WHERE `tablename`=\"" + dbname + "\""
-	fmt.Println(sqlStr)
 	rows, res, err := db.Query(sqlStr)
 
 	if err != nil {
@@ -36,12 +39,12 @@ func databaseParameters(ctx *web.Context, dbname string) string {
 	}
 
 	for _, row := range rows {
-		return (row.Str(res.Map("metadata")))
+		return string(row.Str(res.Map("metadata")))
 	}
 
 	// the Go compiler doesn't detect that we will certainly have returned by
 	// this point and throws an error so we have to return here
-	return "error"
+	return "Error: Malformed input"
 }
 
 func main() {
