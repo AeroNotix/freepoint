533f4f83c19becf383a0432dd75c9388670f7554
AeroNotix <aaron.l.france@gmail.com>
2012-06-20 15:34:53 +0200

    Update connection dialog to enable the population of fields when an item is clicked.

--- ui/connection_dialog.py
+++ ui/connection_dialog.py
@@ -20,15 +20,42 @@ class ConnectionDialog(QtGui.QDialog):
         self.gui = Ui_Dialog()
         self.gui.setupUi(self)
         self.parent = parent
-        self.populate_list()
+        self.connection_map = dict()
+        self.populateList()
 
-    def populate_list(self):
+
+    def populateList(self):
         """
         Function which inserts all new connections into the QListWidget
         """
+
+        self.clearTable()
 
         for connection in self.parent.config.sections():
             host = self.parent.config.get(connection, "host")
             database = self.parent.config.get(connection, "database")
             table = self.parent.config.get(connection, "table")
-            self.gui.listWidget.addItem(QtGui.QListWidgetItem(host+"."+database+"."+"table"))
+            displaystring = QtCore.QString(host+"."+database+"."+table)
+            self.gui.listWidget.addItem(QtGui.QListWidgetItem(displaystring))
+            self.connection_map[displaystring] = connection
+            
+    def populateFields(self, idx):
+        """
+        Populates the text boxes with the correct data for whatever has just
+        been clicked on.
+        """
+        connection = self.connection_map[self.gui.listWidget.item(idx).text()]
+        self.gui.txt_host.setText(self.parent.config.get(connection, "host"))
+        self.gui.txt_database.setText(self.parent.config.get(connection, "database"))
+        self.gui.txt_table.setText(self.parent.config.get(connection, "table"))
+        self.gui.txt_username.setText(self.parent.config.get(connection, "username"))
+        self.gui.txt_password.setText(self.parent.config.get(connection, "password"))
+        self.gui.txt_port.setText(self.parent.config.get(connection, "port"))
+                
+    def clearTable(self):
+        """
+        Clears all the table of the QListWidgetItems
+        """
+
+        for row in range(self.gui.listWidget.count()):
+            self.gui.listWidget.takeItem(row)
