7926fb65fcaedecdc29f3f758b6b2258e5362ffe
Aaron France <aaron.l.france@gmail.com>
2012-06-24 13:22:14 +0200

    Removed server using web.go and added further functionality, error handling and separation of concerns to the net/http based server.

--- settings_server/settings_server.go
+++ settings_server/settings_server.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"connection_details"
 	"encoding/json"
-	"fmt"
+	"errors"
+	"flag"
 	mysql "github.com/ziutek/mymysql/mysql"
 	_ "github.com/ziutek/mymysql/native"
 	"io"
 	"log"
 	"net/http"
-	"strings"
+	"regexp"
 	"time"
 )
+
+type Variable interface{}
 
 type User struct {
 	User     string
@@ -23,8 +27,9 @@ type RouterHandler func(w http.ResponseWriter, req *http.Request)
 
 // Basic RoutingEntry
 type RoutingEntry struct {
-	URL     string
+	URL     *regexp.Regexp
 	Handler RouterHandler
+	Name    string
 }
 
 type SettingsHandler struct{}
@@ -36,20 +41,29 @@ type JSONMessage struct {
 
 var Routes []RoutingEntry = []RoutingEntry{
 	RoutingEntry{
-		URL:     "params",
+		URL:     regexp.MustCompile("^/params/[A-Za-z_-]*/?$"),
 		Handler: databaseParameters,
+		Name:    "Parameters",
 	},
 	RoutingEntry{
-		URL:     "login",
+		URL:     regexp.MustCompile("^/login/$"),
 		Handler: userLogin,
+		Name:    "Login",
 	},
 }
 
+// This is the main 'event loop' for the web server. All requests are
+// sent to this handler, which checks the incoming request against
+// all the routes we have setup if it finds a match it will invoke
+// the handler which is attached to that match.
 func (self *SettingsHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	request := strings.Split(req.URL.Path, "/")
+	request := req.URL.Path
 	for _, route := range Routes {
-		if request[1] == route.URL {
+		matches := route.URL.FindAllStringSubmatch(request, 1)
+		if len(matches) > 0 {
+			log.Println("Matched:", route.Name)
 			route.Handler(w, req)
+			return
 		}
 	}
 }
@@ -77,7 +91,7 @@ func databaseParameters(w http.ResponseWriter, req *http.Request) {
 
 	err := json.NewEncoder(out).Encode(jsonMap)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 }
 
@@ -88,22 +102,7 @@ func userLogin(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	db := mysql.New(
-		"tcp",
-		"",
-		"127.0.0.1:3306",
-		"root",
-		"root",
-		"db_freepoint",
-	)
-
 	w.Header().Set("Content-type", "application/json")
-
-	if err := db.Connect(); err != nil {
-		json.NewEncoder(w).Encode(map[string]bool{"success": false})
-		fmt.Println(err)
-		return
-	}
 
 	username := req.FormValue("User")
 	pass := req.FormValue("Password")
@@ -117,45 +116,98 @@ func userLogin(w http.ResponseWriter, req *http.Request) {
 		Password: pass,
 	}
 
-	// Prepare the statement and execute
-	stmt, _ := db.Prepare("SELECT * FROM tblusers WHERE userid=(?)")
-	resp, _ := stmt.Run(user.User)
-	row, _ := resp.GetRow()
-	if len(row) == 0 {
-		sendJSON(w, "user not found")
+	row, err := getUser(user.User)
+	if err != nil {
+		sendJSON(w, err)
 		return
 	}
+
 	// Create a User instance from the SQL Results.
 	req_user := User{
 		row.Str(1),
 		row.Str(2),
 	}
 
-	var success bool = user == req_user
-	json.NewEncoder(w).Encode(map[string]bool{"success": success})
+	// if we've got here, we either are logged in or not.
+	sendJSON(w, user == req_user)
 	return
 }
 
-func sendJSON(w http.ResponseWriter, message string) {
-	json.NewEncoder(w).Encode(map[string]string{"error": message})
+// Sends JSON using the message as the payload. Determines the
+// underlying type and uses some specific handling for that type.
+func sendJSON(w http.ResponseWriter, message interface{}) {
+
+	// type switch on the message interface
+	switch message.(type) {
+	default:
+		return
+	case error:
+		json.NewEncoder(w).Encode(map[string]string{
+			"Error": message.(error).Error(),
+		})
+	case string:
+		json.NewEncoder(w).Encode(map[string]string{
+			"Error": message.(string),
+		})
+	case bool:
+		json.NewEncoder(w).Encode(map[string]bool{
+			"Success": message.(bool),
+		})
+	}
 	return
+}
+
+func createConnection() mysql.Conn {
+	db := mysql.New(
+		"tcp",
+		"",
+		connection_details.IP,
+		connection_details.User,
+		connection_details.Password,
+		connection_details.Database,
+	)
+	return db
+}
+
+func getUser(user string) (mysql.Row, error) {
+
+	db := createConnection()
+	defer db.Close()
+	if err := db.Connect(); err != nil {
+		return nil, err
+	}
+
+	// Prepare the statement and execute
+	stmt, err := db.Prepare("SELECT * FROM tblusers WHERE userid=(?)")
+	if err != nil {
+		return nil, err
+	}
+	resp, err := stmt.Run(user)
+	if err != nil {
+		return nil, err
+	}
+	row, err := resp.GetRow()
+	if err != nil {
+		return nil, err
+	}
+	if len(row) == 0 {
+		return nil, errors.New("No users found with that userid")
+	}
+
+	return row, nil
 }
 
 func main() {
-	http.HandleFunc("/params/", databaseParameters)
-
+	var addr string
+	flag.StringVar(&addr, "port", ":12345",
+		"The port on which the server should run",
+	)
+	flag.Parse()
+	log.Println("Serving on", addr)
 	s := http.Server{
-		Addr:        ":12345",
+		Addr:        addr,
 		Handler:     &SettingsHandler{},
 		ReadTimeout: 30 * time.Second,
 	}
-
-	err := s.ListenAndServe()
-	if err != nil {
-		fmt.Println(err)
-	}
-	err = http.ListenAndServe(":12345", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	s.ListenAndServe()
 }
