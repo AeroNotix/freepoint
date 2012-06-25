8194af7fbcce9dabc80f8e04ae16390174942514
AeroNotix <aaron.l.france@gmail.com>
2012-06-25 16:34:18 +0200

    Docstring changes

--- settings_server/src/settingsserver/routing.go
+++ settings_server/src/settingsserver/routing.go
@@ -2,14 +2,23 @@ package settingsserver
 
 import (
 	"regexp"
-"net/http"
+	"net/http"
 )
 
 // This type defines what signatures of functions can be used
 // as request handlers.
 type RouterHandler func(w http.ResponseWriter, req *http.Request)
 
-// Basic RoutingEntry
+// RoutingEntry takes a URL, a RouterHandler and a String
+// we define the URL as a regex because this way we can
+// have a rich url interface to our application.
+//
+// For example we can have something like:
+// domain/getdb/[A-Za-z]/ automatically retrieve the database
+// which the group matches without parsing the url manually.
+// RouterHandler is a type which is defined in this package.
+// Name simply allows us to have a 'pretty' version of the function
+// name.
 type RoutingEntry struct {
 	URL     *regexp.Regexp
 	Handler RouterHandler
