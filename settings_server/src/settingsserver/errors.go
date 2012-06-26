c2690ed0a2772616a173ca932f4fe2a4590e5dfa
AeroNotix <aaron.l.france@gmail.com>
2012-06-26 13:52:13 +0200

    Added an error module to the go package so we can have somewhat idiomatic Go error handling

---
+++ settings_server/src/settingsserver/errors.go
@@ -0,0 +1,21 @@
+package settingsserver
+
+
+type AppError struct {
+	errorString string
+	errorCode int
+}
+
+func (e AppError) Error() string {
+	return e.errorString
+}
+
+func (e AppError) Code() int {
+	return e.errorCode
+}
+
+var (
+	LoginError = AppError{"Login Failure", 1}
+	RequestError = AppError{"Incorrect request mode", 2}
+	InvalidTable = AppError{"Table not found", 3}
+)
