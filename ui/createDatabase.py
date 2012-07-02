ec4ed5e68c096b47883e80c6bab056946b023147
Aaron France <aaron.l.france@gmail.com>
2012-07-02 23:54:10 +0200

    Added a really hacky implementation of the group widget switcher for the add new database UI. I really don't like this implementation because it has really implicit knowledge of the widgets and what they are. For example, adding a new row type will be a great deal of effort.

--- ui/createDatabase.py
+++ ui/createDatabase.py
@@ -2,23 +2,25 @@ from PyQt4 import QtGui, QtCore
 from qtsqlviewer.ui.create_new_table_UI import Ui_Dialog
 
 
-formA = [
-    QtGui.QComboBox,
-    QtGui.QComboBox,
-    QtGui.QComboBox,
-    QtGui.QComboBox,
-    QtGui.QComboBox,
-    QtGui.QComboBox
-    ]
-
 class CreateNewTable(QtGui.QDialog):
     def __init__(self, parent=None):
         super(CreateNewTable, self).__init__(parent)
         self.gui = Ui_Dialog()
         self.gui.setupUi(self)
+        self.gui.tabWidget.tabBar().setVisible(False)
+        
+    def changeFieldDescriptions(self, index):
+        """
+        """
+        self.gui.tabWidget.setCurrentIndex(~index-1)
 
-    def changeFieldDescriptions(self):
-        for i in range(self.gui.verticalLayout_2.count()):
-            self.gui.verticalLayout_2.itemAt(i).widget().close()
-        for item in formA:
-            self.gui.verticalLayout_2.addWidget(item(self.gui.groupBox_2))
+    def acceptFieldAdd(self):
+        print self.sender().parent().objectName()
+
+
+
+
+
+
+
+
