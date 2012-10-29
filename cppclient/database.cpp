26ba78a4a8d4198a937c1db64208463fd4e3e237
AeroNotix <aaron.l.france@gmail.com>
2012-10-29 12:28:33 +0000

    Further work on the C++ client, whitespace and style changes

--- cppclient/database.cpp
+++ cppclient/database.cpp
@@ -1,21 +1,24 @@
 #include "database.h"
 #include <QtGui>
 
-Database::Database(QWidget *parent, QString user, QString passwd,QString using_db, QString table, QString URL)
-{
-  Parent = parent;
-  User = user;
-  Password = passwd;
-  UsingDB = using_db;
-  TableName = table;
-  BaseURL = URL;
-  ParamURL = BaseURL+"getdb/";
-  LoginURL = BaseURL+"login/";
-  UpdateURL = BaseURL+"update/";
-  InsertURL = BaseURL+"insert/";
+Database::Database
+(QWidget *parent, QString user, QString passwd,QString using_db, QString table, QString URL) {
+    Parent = parent;
+    User = user;
+    Password = passwd;
+    UsingDB = using_db;
+    TableName = table;
+    BaseURL = URL;
+    ParamURL = BaseURL+"getdb/";
+    LoginURL = BaseURL+"login/";
+    UpdateURL = BaseURL+"update/";
+    InsertURL = BaseURL+"insert/";
 }
 
-Database::~Database()
-{
+Database::~Database() {
+    return;
+}
+
+void Database::Close() {
     return;
 }
