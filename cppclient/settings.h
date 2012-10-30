dafa8b91eacea0af057e73b8736a01819ce84629
AeroNotix <aaron.l.france@gmail.com>
2012-10-30 12:20:39 +0000

    Added guard clauses

--- cppclient/settings.h
+++ cppclient/settings.h
@@ -1,2 +1,7 @@
-const char* SERVERURL = "http://16.55.133.115:12345";
-const char* LOGINURL = "http://16.55.133.115:12345/login/";
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#define SERVERURL "http://16.55.133.115:12345"
+#define LOGINURL "http://16.55.133.115:12345/login/"
+
+#endif
