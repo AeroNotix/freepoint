dfad38d1b9ce21c0facd52bcb0f56008fd432d6b
Aaron <aaron.l.france@gmail.com>
2012-11-04 16:06:09 +0100

    Changed SelectDelegate function signature

--- cppclient/delegates.cpp
+++ cppclient/delegates.cpp
@@ -4,6 +4,29 @@
 
 #include "delegates.h"
 
+QItemDelegate* SelectDelegate(QString choice, QMainWindow *parent) {
+
+	static QMap<QString, int> choiceMap;
+	choiceMap.insert("BOOL", 0);
+	choiceMap.insert("CHOICE", 1);
+	choiceMap.insert("TIME", 2);
+	choiceMap.insert("DATE", 3);
+
+	int val = choiceMap.value(choice, -1);
+
+	switch (val) {
+	case 0:      // BOOL
+	case 1:      // CHOICE
+		return new ComboDelegate(QStringList({"Yes","No"}), parent);
+	case 2:      // TIME
+		return new TimeDelegate(parent);
+	case 3:      // DATE
+		return new DateDelegate(parent);
+	default:
+		return new QItemDelegate(parent);
+	}
+}
+
 QWidget* ComboDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &option,
                                          const QModelIndex &index) const
 {
