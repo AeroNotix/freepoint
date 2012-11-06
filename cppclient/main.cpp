764f6c6935ff2e22f78dd6d176b722586f52ecae
AeroNotix <aaron.l.france@gmail.com>
2012-11-06 11:32:05 +0100

    Throw exception if server config is missing. We will need to create a dialog to create the initial connection file if one does not exist

--- cppclient/main.cpp
+++ cppclient/main.cpp
@@ -9,21 +9,23 @@
   values which should be  ble to be changed at runtime hence not using
   #defines.
  */
+#include <stdexcept>
+
 #include <QtGui/QApplication>
 #include <QDir>
 
 #include "mainwindow.h"
 #include "table_tools.h"
 #include "settings.h"
-#include "cxn_setup.h"
 
 int main(int argc, char *argv[])
 {
 	// we need the cwd for two things, might as well store it.
 	QDir cwd = sgetcwd();
 	if (!InitializeSettings(appendDir(cwd, "server.json"))) {
-		// Where we'll open the manage connection dialog etc.
-	}
+        // handle this here. For now we'll just quit
+        throw std::runtime_error("Cannot find/parse server configuration");
+    }
 	QDir resource = appendDir(cwd, "resources/resource.rcc");
 	QResource::registerResource(resource.path());
     QApplication a(argc, argv);
