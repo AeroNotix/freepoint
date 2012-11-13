0633d007bda9b8cc97cf318485eb7046aaf701c6
AeroNotix <aaron.l.france@gmail.com>
2012-11-13 16:45:09 +0100

    Finished up the managedialog by adding a CustomListWidget to help us identify which rows we're editing.

--- cppclient/managedialog.h
+++ cppclient/managedialog.h
@@ -21,12 +21,44 @@ public QDialog {
   explicit ManageConnectionDialog(MainWindow *parent = 0);
   virtual ~ManageConnectionDialog() {
     delete ui;
+    for (int x = 0; x < listwidgets.size(); ++x)
+        delete listwidgets[x];
   }
   virtual void accept(void);
   virtual void reject(void);
+  
+private slots:
+    void fillList();
+    void populateFields(QListWidgetItem *item);
+    void deleteRow();
 
- private:
+private:
+    void WriteConfig();
+
     MainWindow *parent;
     Ui_ConnectionsDialog *ui;
+    QList<QListWidgetItem*> listwidgets;
+};
+
+/*
+  This was created because QListWidgetItem does not inherit from QObject
+  and thus doesn't have the setObjectName function. We can use this on
+  other classes to have an internal name or piece of data associated
+  with a widget.
+*/
+class CustomListWidget :
+    public QListWidgetItem {
+
+public:
+    explicit CustomListWidget(QString s)
+        : QListWidgetItem(s) {}
+    void setObjectName(QString s) {
+        o = s;
+    }
+    const QString objectName() const {
+        return o;
+    }
+private:
+    QString o;
 };
 #endif
