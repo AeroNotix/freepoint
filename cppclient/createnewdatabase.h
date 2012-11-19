433e075d1ae31575c03f58257ceece9c0f9eac6c
AeroNotix <aaron.l.france@gmail.com>
2012-11-19 15:28:39 +0100

    Further documentation changes.

--- cppclient/createnewdatabase.h
+++ cppclient/createnewdatabase.h
@@ -20,12 +20,12 @@ class CreateNewDatabase :
 public:
 
     enum {
-        text,
-        choice,
-        date,
-        time,
-        group,
-        currency
+        text,      /*!< Text field. */
+        choice,    /*!< Choice field. */
+        date,      /*!< Date field. */
+        time,      /*!< Time field. */
+        group,     /*!< Group field. */
+        currency   /*!< Currency field. */
     };
 
     explicit CreateNewDatabase(MainWindow *parent = 0);
@@ -33,33 +33,37 @@ public:
         for (int x = 0; x < list_items.size(); ++x)
             delete list_items[x];
     }
-    /*
+    /**
      * @brief Simply accepts the dialog without creating a database or prompting
      * the user to see if they are sure about quitting.
      */
     virtual void accept(void);
-    /*
+    /**
      * @brief Simply rejectss the dialog without creating a database it will
      * prompt the user to confirm before closing.
      */
     virtual void reject(void);
 
 public slots:
-    /*
+    /**
      * @brief changeFieldDescriptions is fired when the fieldButtonGroup is changed
      * we N = (~N-1) because the field ID numbers are negative indexes starting
      * from -2 for some bloody reason. Dafuq?
      * @param i this is the index of the fieldtype which was selected.
      */
     void changeFieldDescriptions(int i);
-    /*
+    /**
      * @brief acceptFieldAdd is fired on a StackedWidget page. It will determine
      * which page is currently active and create a row entry from the widgets
      * on that page.
      */
     void acceptFieldAdd();
+    /**
+     * @brief deletes the currently selected row from the display port and
+     * from the internal JSON string map.
+     */
     void DeleteSelectedRow();
-    /*
+    /**
      * @brief CreateTable creates the JSON string from the user-created rows and fires
      * off a request to the database.
      */
