114023fa8704c23237a7351815598686f1158d92
AeroNotix <aaron.l.france@gmail.com>
2012-11-19 15:12:22 +0100

    Added Documentation to the CXNSetup class.

--- cppclient/cxn_setup.h
+++ cppclient/cxn_setup.h
@@ -1,3 +1,4 @@
+/// The dialog with which to add new connections to a user's config.
 #ifndef CXN_SETUP_H
 #define CXN_SETUP_H
 
@@ -11,24 +12,43 @@
 #include "mainwindow.h"
 #include "ui_cxn_setup.h"
 
+
+/**
+ * @brief Creates a dialog window to add new connections.
+ */
 class CXNSetup :
     public QDialog {
 
     Q_OBJECT
 
 public:
-
     enum WindowMode {
-        StandAlone,
-        Dialog
+        StandAlone, /*!< Sets the mode to kill the application when the dialog is closed. */
+        Dialog      /*!< Sets the mode to a modal dialog. */
     };
 
+   /**
+    * @brief Constructor.
+    * @param mode selects which type of window to use. This determines
+    * how the reject() method is invoked. It will either kill the
+    * application (CXNSetup::StandAlone) or it will simply reject
+    * (CXNSetup::Dialog).
+    * @param parent this is a pointer to the parent object. All QObjects
+    * need a pointer to their parent so they can set who they are managed
+    * by.
+    */
     explicit CXNSetup(CXNSetup::WindowMode mode, MainWindow *parent = 0);
     virtual ~CXNSetup() {
         delete ui;
     }
 
+   /**
+    * @brief Accepts the dialog.
+    */
     void accept();
+   /**
+    * @brief Rejects the dialog.
+    */
     void reject();
 
 private:
