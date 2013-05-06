16d3d0f973c93dd771fcabc2a06eb97600cc0679
AeroNotix <aaron.l.france@gmail.com>
2013-05-06 13:32:57 +0200

    Updated docstrings.

--- cppclient/settings.h
+++ cppclient/settings.h
@@ -7,14 +7,14 @@ class Settings {
 public:
     Settings(QString filename);
     void OpenSettings(QString filename);
-    static QString SERVERURL;
-    static QString LOGINURL;
-    static QString PARAMURL;
-    static QString UPDATEURL;
-    static QString INSERTURL;
-    static QString DELETEURL;
-    static QString CREATEURL;
-	static QString USERURL;
+    static QString SERVERURL; /*!< The base URL for which the server should be accessed */
+    static QString LOGINURL;  /*!< The URL where the login endpoint resides */
+    static QString PARAMURL;  /*!< The URL where the database parameters reside */
+    static QString UPDATEURL; /*!< The URL where updates to the database are requested */
+    static QString INSERTURL; /*!< The URL where inserts to the database are requested */
+    static QString DELETEURL; /*!< The URL where deletes to the database are requested */
+    static QString CREATEURL; /*!< The URL where creates to the database are requested */
+	static QString USERURL;   /*!< The URL where new users are created via */
 };
 
 bool InitializeSettings(QDir filename);
